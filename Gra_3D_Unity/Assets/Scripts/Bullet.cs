367bcb588949c09305137d9c6847f6e4bef64bc3
mat <mateusz.paszek1.3@gmail.com>
2018-05-09 09:32:20 +0200

    #strzał

---
+++ Gra_3D_Unity/Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	private float timer = 0.0f;
+
+	void Update() {
+		timer += Time.deltaTime;
+		if(timer > 4.0f) {
+			Destroy(gameObject);
+		}
+	}
+
+}
---
+++ Gra_3D_Unity/Assets/Scripts/Shoot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Shoot : MonoBehaviour
+{
+
+    public Rigidbody bullet;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody clone = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
+            Vector3 fwd = transform.TransformDirection(Vector3.forward);
+            clone.AddForce(fwd * 1500f);
+        }
+    }
+}
