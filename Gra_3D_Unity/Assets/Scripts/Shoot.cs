2d9ca0a6b0437950c84bcde03a2e8187a151167b
mat <mateusz.paszek1.3@gmail.com>
2018-05-09 10:26:39 +0200

    #komentarze

--- Gra_3D_Unity/Assets/Scripts/Shoot.cs
+++ Gra_3D_Unity/Assets/Scripts/Shoot.cs
@@ -7,14 +7,14 @@ public class Shoot : MonoBehaviour
 
     public Rigidbody bullet;
 
-    // Update is called once per frame
-    void Update()
+    
+    void Update() //update - strzelanie w każdej chwili
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1")) //pocisk wyleci kiedy naciśniemy lewy "ctrl"
         {
-            Rigidbody clone = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
-            Vector3 fwd = transform.TransformDirection(Vector3.forward);
-            clone.AddForce(fwd * 1500f);
+            Rigidbody clone = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody; //pozycja i rotacja obiektu
+            Vector3 fwd = transform.TransformDirection(Vector3.forward);                                //wektor przesunięcia skierowany do przodu
+            clone.AddForce(fwd * 1500f); //przekazanie energii obiektowi
         }
     }
 }
