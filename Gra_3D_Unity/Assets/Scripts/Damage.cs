9d43cfb4e5bb71db5d8f0329c5cb85b06f746d8c
mat <mateusz.paszek1.3@gmail.com>
2018-05-11 09:53:52 +0200

    #5

---
+++ Gra_3D_Unity/Assets/Scripts/Damage.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine; 
+using System.Collections;
+ 
+ public class EnemyHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int curHealth = 100;
+
+    //initialization 
+    void Start() { }
+
+    // Update 
+    void Update()
+    {
+        if (curHealth < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Bullet")
+        {
+            curHealth -= 20;
+            Destroy(col.other);
+        }
+    }
+}
