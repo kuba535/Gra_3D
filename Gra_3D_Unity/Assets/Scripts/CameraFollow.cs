967d0bf7e492e73799712bb238afe346832a3dcb
kuba535 <38734200+kuba535@users.noreply.github.com>
2018-05-14 19:49:14 +0200

    CameraFollow
    Dodałem podążanie kamery za graczem

---
+++ Gra_3D_Unity/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;            // Obiekt za którym podąża kamera
+    public float smoothing = 5f;        // Prędkość kamery
+
+    Vector3 offset;                     // Pozycja początkowa obiektu
+
+    void Start()
+    {
+        // Ustaw różnicę pomiędzy kamerą, a obiektem
+        offset = transform.position - target.position;
+    }
+
+    void FixedUpdate()
+    {
+        //Stwórz pozycje za jaką ma podążać kamera
+        Vector3 targetCamPos = target.position + offset;
+
+        // Delikatnie przemieść kamerę
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+}
