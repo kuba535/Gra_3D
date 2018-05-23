b911120cb38f08604cce1df6772f4e7b91f7061e
Aleksander <aleksander@tuinel.eu>
2018-05-23 18:04:00 +0200

    Atak_kobiet ;) Naprawa Enemy health i bullet.

--- Gra_3D_Unity/Assets/Scripts/Player/Bullet.cs
+++ Gra_3D_Unity/Assets/Scripts/Player/Bullet.cs
@@ -1,15 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Bullet : MonoBehaviour {
+public class Bullet : MonoBehaviour
+{
+    public int Damage = 10;
 
-	private float timer = 0.0f;
+    
 
-	void Update() {
-		timer += Time.deltaTime;
-		if(timer > 4.0f) {
-			Destroy(gameObject);
-		}
-	}
+    GameObject enemy;
+    New_Enemy_Health enemyHealth;
+    
+    void Awake()
+    {
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        enemyHealth = enemy.GetComponent<New_Enemy_Health>();
+    }
 
+    void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.tag == "Enemy")
+        {
+            Attack();
+        }
+        if(other.gameObject.tag != "Player" )
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Attack()
+    {
+        if (enemyHealth.currentHealth > 0)
+        {
+            enemyHealth.TakeDamage(Damage);
+        }
+
+        Destroy(gameObject);
+    }
 }
