c3f42fb99d6ae6328062386bac402669d9cfe988
Lukasz9412 <lukaszputoninpg@gmail.com>
2018-05-23 12:33:10 +0200

    V03.04ŁP Fixed: Script enemy attack
    Warning:
    There is no enemy health script so I cannot test my script properly

--- Gra_3D_Unity/Assets/Scripts/Enemies/EnemyAttack.cs
+++ Gra_3D_Unity/Assets/Scripts/Enemies/EnemyAttack.cs
@@ -1,29 +1,31 @@
-﻿/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class EnemyAttack : MonoBehaviour {
 
+public class EnemyAttack : MonoBehaviour
+{
     public float timeBetweenAttacks = 0.5f;     // The time in seconds between each attack.
     public int attackDamage = 10;               // The amount of health taken away per attack.
 
 
-   // Animator anim;                              // Reference to the animator component.
+    //Animator anim;                              // Reference to the animator component.
     GameObject player;                          // Reference to the player GameObject.
     PlayerHealth playerHealth;                  // Reference to the player's health.
-    EnemyHealth enemyHealth;                    // Reference to this enemy's health.
+   // EnemyHealth enemyHealth;                    // Reference to this enemy's health.
     bool playerInRange;                         // Whether player is within the trigger collider and can be attacked.
     float timer;                                // Timer for counting up to the next attack.
 
-    // Use this for initialization
-    void Awake () {
+
+    void Awake()
+    {
         // Setting up the references.
         player = GameObject.FindGameObjectWithTag("Player");
-        playerHealth = player.GetComponent();
-        enemyHealth = GetComponent();
-        //anim = GetComponent();
-
+        playerHealth = player.GetComponent<PlayerHealth>();
+      //  enemyHealth = GetComponent<EnemyHealth>();
+        //anim = GetComponent<Animator>();
     }
+
+
     void OnTriggerEnter(Collider other)
     {
         // If the entering collider is the player...
@@ -33,6 +35,8 @@ public class EnemyAttack : MonoBehaviour {
             playerInRange = true;
         }
     }
+
+
     void OnTriggerExit(Collider other)
     {
         // If the exiting collider is the player...
@@ -44,37 +48,37 @@ public class EnemyAttack : MonoBehaviour {
     }
 
 
-    // Update is called once per frame
-    void Update () {
+    void Update()
+    {
         // Add the time since Update was last called to the timer.
         timer += Time.deltaTime;
 
         // If the timer exceeds the time between attacks, the player is in range and this enemy is alive...
-        if (timer & gt;= timeBetweenAttacks & amp; &amp; playerInRange & amp; &amp; enemyHealth.currentHealth & gt; 0)
+        if (timer >= timeBetweenAttacks && playerInRange /*&& enemyHealth.currentHealth > 0*/)
         {
             // ... attack.
             Attack();
         }
 
         // If the player has zero or less health...
-        if (playerHealth.currentHealth & lt;= 0)
+        if (playerHealth.currentHealth <= 0)
         {
             // ... tell the animator the player is dead.
-            anim.SetTrigger("PlayerDead");
+           // anim.SetTrigger("PlayerDead");
         }
-
     }
+
+
     void Attack()
     {
         // Reset the timer.
         timer = 0f;
 
         // If the player has health to lose...
-        if (playerHealth.currentHealth & gt; 0)
+        if (playerHealth.currentHealth > 0)
         {
             // ... damage the player.
             playerHealth.TakeDamage(attackDamage);
         }
-
     }
-    */
+}
