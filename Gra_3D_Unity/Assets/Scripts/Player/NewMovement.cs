70183bb9e3f11e786c6fb2f51d67a4783592c448
Panda the Little <38527153+pandathelittle@users.noreply.github.com>
2018-05-24 00:07:33 +0200

    Zmiana poruszania się
    Niestety nie udało mi się do końca dokończyć. Muszę jeszcze skonfgurować kierunek ruchu.

---
+++ Gra_3D_Unity/Assets/Scripts/Player/NewMovement.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewMovement : MonoBehaviour
+{
+
+    // Use this for initialization
+    public float speed = 6f;            // The speed that the player will move at.
+
+    Vector3 movement;                   // The vector to store the direction of the player's movement.
+    Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+    int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
+    float camRayLength = 100f;          // The length of the ray from the camera into the scene.
+
+    void Awake()
+    {
+
+
+        // Set up references.
+        floorMask = LayerMask.GetMask("Floor");
+        playerRigidbody = GetComponent<Rigidbody>();
+        movement = transform.position;
+    }
+
+
+    void FixedUpdate()
+    {
+        // Move the player around the scene.
+        Move();
+        Turning();
+
+    }
+
+    void Move()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            movement = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            movement = new Vector3(movement.x, transform.position.y, movement.z);
+        }
+        transform.position = Vector3.MoveTowards(transform.position, movement, speed * Time.deltaTime);
+    }
+
+    void Turning()
+    {
+        // Create a ray from the mouse cursor on screen in the direction of the camera.
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Create a RaycastHit variable to store information about what was hit by the ray.
+        RaycastHit floorHit;
+
+        // Perform the raycast and if it hits something on the floor layer...
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            // Create a vector from the player to the point on the floor the raycast from the mouse hit.
+            Vector3 playerToMouse = floorHit.point - transform.position;
+
+            // Ensure the vector is entirely along the floor plane.
+            playerToMouse.y = 0f;
+
+            // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+
+            // Set the player's rotation to this new rotation.
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+
+
+}
