92a424933b9ce3daceb74e19dc5ab7755d8fc28a
mat <mateusz.paszek1.3@gmail.com>
2018-05-28 13:58:54 +0200

    #pocisk

--- Gra_3D_Unity/Assets/Scripts/Player/Bullet.cs
+++ Gra_3D_Unity/Assets/Scripts/Player/Bullet.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+﻿/*using UnityEngine;
 using System.Collections;
 
 public class Bullet : MonoBehaviour
@@ -24,6 +24,48 @@ public class Bullet : MonoBehaviour
             Attack();
         }
         if(other.gameObject.tag != "Player" )
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Attack()
+    {
+        if (enemyHealth.currentHealth > 0)
+        {
+            enemyHealth.TakeDamage(Damage);
+        }
+
+        Destroy(gameObject);
+    }
+}
+*/
+using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+    public int Damage = 10;
+
+
+
+    GameObject enemy;
+    New_Enemy_Health enemyHealth;
+
+    void Awake()
+    {
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        enemyHealth = enemy.GetComponent<New_Enemy_Health>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.tag == "Enemy")
+        {
+            Attack();
+        }
+        if (other.gameObject.tag != "Player")
         {
             Destroy(gameObject);
         }
