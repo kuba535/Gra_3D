3c5d86594ea6dd417b36dfcba84ec4e10560f970
Panda the Little <38527153+pandathelittle@users.noreply.github.com>
2018-05-30 20:49:18 +0200

    Revert "Merge branch 'master' of https://github.com/kuba535/Gra_3D"
    This reverts commit 067e90bcdef91aa283ad40109141a42b780e307e, reversing
    changes made to af2faac034272a90c445ef5aea7d5a4f201beb71.

--- Gra_3D_Unity/Assets/Scripts/Enemies/EnemyMovment.cs
+++ Gra_3D_Unity/Assets/Scripts/Enemies/EnemyMovment.cs
@@ -16,6 +16,7 @@ public class EnemyMovment : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player").transform;
         //Sprawdzanie czy obiekt istnieje, żeby mógł podążać za graczem?
         nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
     }
 
     // Update is called once per frame
--- Gra_3D_Unity/Assets/Scripts/Player/Bullet.cs
+++ Gra_3D_Unity/Assets/Scripts/Player/Bullet.cs
@@ -1,43 +1,17 @@
 ﻿/*using UnityEngine;
 using System.Collections;
 
-public class Bullet : MonoBehaviour
-{
-    public int Damage = 10;
+public class Bullet : MonoBehaviour {
 
-    
+	private float timer = 0.0f;
 
-    GameObject enemy;
-    New_Enemy_Health enemyHealth;
-    
-    void Awake()
-    {
-        enemy = GameObject.FindGameObjectWithTag("Enemy");
-        enemyHealth = enemy.GetComponent<New_Enemy_Health>();
-    }
+	void Update() {
+		timer += Time.deltaTime;
+		if(timer > 4.0f) {
+			Destroy(gameObject);
+		}
+	}
 
-    void OnCollisionEnter(Collision other)
-    {
-
-        if (other.gameObject.tag == "Enemy")
-        {
-            Attack();
-        }
-        if(other.gameObject.tag != "Player" )
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    void Attack()
-    {
-        if (enemyHealth.currentHealth > 0)
-        {
-            enemyHealth.TakeDamage(Damage);
-        }
-
-        Destroy(gameObject);
-    }
 }
 */
 using UnityEngine;
